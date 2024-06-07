bd388aa9445a342d3d46231abf02cd8ab23c08a7
l.nawrocki <lnawrocki84@gmail.com>
2024-06-07 11:29:08 +0200

    Initial commit

---
+++ 01-pierwszy-plik.py
@@ -0,0 +1,7 @@
+name = input("Jak masz na imię? ")
+surname = input("Jak masz na nazwisko? ")
+age = input("Ile masz lat? ")
+
+print("Imię: " + name)
+print("Nazwisko: " + surname)
+print("Wiek: " + age)
---
+++ 02-operacje-liczbowe.py
@@ -0,0 +1,19 @@
+a = 5
+# b = 2
+
+# print(a + b)
+# print(a - b)
+# print(a * b)
+# print(a / b)
+# print(a % b)
+
+# a = a + 1
+# print(a)
+# a += 1
+# print(a)
+# a -= 1
+# print(a)
+# a *= 2
+# print(a)
+a /= 2
+print(a)
