6442656ddd23e2188be6323113a26d03fa3fb89c
ŁukaszN <lnawrocki84@gmail.com>
2024-06-11 20:44:05 +0200

    Do 02-26 exercises

---
+++ 02.26-Cwiczenia-zestaw-nr-2.py
@@ -0,0 +1,28 @@
+# 1. Stwórz listę, którą wypełnisz 10 liczbami
+# całkowitymi i zwróć sumę wszstkich liczb z tej listy.
+
+# 2. Stwórz listę, która będzie zawierać 10 słó.
+# Następnie stwórz drugą listę, która będzie zawierać
+# tylko te słowa, które mają więcej niż 5 liter.
+
+#3. Stwórz listę, którą wypełnisz 10 liczbami całkowitymi. Znajdź najmniejszą i najwieksząliczbę.
+
+#4. Stwórz listę, którą wypełnisz imionami - po 5 imion w każdej z list. Zadbaj o to, by niektóre imiona się powtarzały(powtarzały między listami). Wyświetl te imiona.
+
+#5. Korzystająć z list z poprzedniego ćwiczenia, wyświetl tylko te imiona, które są obecne w pierwszej liście.
+
+#6. Stwórz słownik. który będzie przechowywał informacje o krajach i ich stolicach. Dodaj do niego 5 elementów.
+#Wyświetl dane ze słowika w formacie kraj - stolica czyli np. Polska - Warszawa.
+
+#7. Posortuj słownik z poprzedniego ćwiczenia w kolejności alfabetycznej.
+
+#8. Stwórz program, który prosi użytkownika o wpisanie tekstu
+# w konsoli, a następnie utwórz słownik, w którym kluczami będą
+# słowa występujące w tym tekście, a wartościami liczby wystąpień każdego słowa.
+# Wyświetl zawartość słwnika.
+
+# Jak nauczyć się programowania
+# Jak: 1
+# nauczyć: 1
+# się: 1
+# programowania: 1
